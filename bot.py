e689ee861d25978f47afb49f6b8b2c35881e2fa3
Ilya Shchuka <ilya.shchuka@yandex.ru>
2020-06-29 03:20:45 +0300

    First commit: style transfer, simple bot

---
+++ bot.py
@@ -0,0 +1,70 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import os
+from utils import load_img
+from torchvision.utils import save_image
+from model import encoders, decoders
+from wct import WCT
+
+
+TOKEN = os.environ['BOT_TOKEN']
+WEBHOOK_HOST = 'https://stylus-bot.herokuapp.com/'
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+keyboard_markup = types.ReplyKeyboardMarkup()
+btns_text = ('Загрузить содержание', 'Загрузить стиль', 'Применить стиль')
+for text in btns_text:
+    keyboard_markup.row(types.KeyboardButton(text))
+
+
+model = WCT(0.25, encoders, decoders)
+model.eval()
+
+
+@dp.message_handler(commands=['start'])
+async def start_cmd_handler(message: types.Message) -> None:
+    await message.reply("Привет! Я StylusBot - умею применять любой стиль к любой картинке.\n"
+                        "Для загрузки содержания и стиля воспользуйтесь меню. "
+                        "А затем нажмите кнопку 'Применить стиль'.", reply_markup=keyboard_markup)
+
+TYPE = 'content'
+@dp.message_handler(text='Загрузить содержание')
+async def all_msg_handler(message: types.Message):
+    await message.reply('Загрузите картинку', reply_markup=keyboard_markup)
+    global TYPE
+    TYPE = 'content'
+
+
+@dp.message_handler(text='Загрузить стиль')
+async def all_msg_handler(message: types.Message):
+    await message.reply('Загрузите стиль', reply_markup=keyboard_markup)
+    global TYPE
+    TYPE = 'style'
+
+
+@dp.message_handler(text='Применить стиль')
+async def all_msg_handler(message: types.Message):
+    await message.reply('Применяю стиль...', reply_markup=keyboard_markup)
+    img_content = load_img('content.jpg')
+    img_style = load_img('style.jpg', new_size=256)
+    result = model(img_content.unsqueeze(0), img_style.unsqueeze(0))
+    save_image(result, 'result.jpg')
+    await bot.send_photo(message.chat.id, open('result.jpg', 'rb'))
+
+
+@dp.message_handler(content_types=['photo'])
+async def handle_photo(message):
+    await message.photo[-1].download(f'{TYPE}.jpg')
+
+
+if __name__ == '__main__':
+    executor.start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                           host=WEBAPP_HOST, port=WEBAPP_PORT)
---
+++ wct.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+
+class WCT(nn.Module):
+  def __init__(self, alpha, encoders, decoders):
+    super().__init__()
+    self.alpha = alpha
+    self.encoders = encoders
+    self.decoders = decoders
+
+  def stylize(self, feat_c, feat_s):
+    def whiten_and_color(feat_c, feat_s):
+      c_size = feat_c.size()
+      c_mean = torch.mean(feat_c, 1)
+      c_mean = c_mean.unsqueeze(1).expand(c_size)
+      feat_c = feat_c - c_mean
+
+      content_conv = (feat_c @ feat_c.t()).div(c_size[1] - 1) + \
+                      torch.eye(c_size[0])
+      _ , c_e, c_v = torch.svd(content_conv, some=False)
+
+      k_c = c_size[0]
+      for i in range(c_size[0]):
+          if c_e[i] < 0.00001:
+              k_c = i
+              break
+
+      s_size = feat_s.size()
+      s_mean = torch.mean(feat_s, 1)
+      feat_s = feat_s - s_mean.unsqueeze(1).expand(s_size)
+      style_conv = (feat_s @ feat_s.t()).div(s_size[1] - 1)
+      _, s_e, s_v = torch.svd(style_conv, some=False)
+
+      k_s = s_size[0]
+      for i in range(s_size[0]):
+        if s_e[i] < 0.00001:
+          k_s = i
+          break
+
+      c_d = 1 / torch.sqrt(c_e[0:k_c])
+      step1 = c_v[:,0:k_c] @ torch.diag(c_d)
+      step2 = step1 @ c_v[:,0:k_c].t()
+      whiten_feat_c = step2 @ feat_c
+
+      s_d = torch.sqrt(s_e[0:k_s])
+      target = s_v[:,0:k_s] @ torch.diag(s_d) @ s_v[:,0:k_s].t() @ whiten_feat_c
+      target = target + s_mean.unsqueeze(1).expand_as(target)
+      return target
+
+    C = feat_c.size(0)
+
+    target = whiten_and_color(feat_c.view(C, -1), feat_s.view(C, -1))
+
+    target = target.view_as(feat_c)
+    result = self.alpha * target + (1.0 - self.alpha) * feat_c
+    return result.unsqueeze(0)
+
+  def forward(self, img_content, img_style):
+    with torch.no_grad():
+      for enc, dec in zip(self.encoders, self.decoders):
+        content = enc(img_content).squeeze(0)
+        style = enc(img_style).squeeze(0)
+        content_style = self.stylize(content.squeeze(0), style.squeeze(0))
+        img_content = dec(content_style)
+    return img_content.squeeze(0)
