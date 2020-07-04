6b8060f97f157acf0085723a6d86c66da9fc483a
Ilya Shchuka <ilya.shchuka@yandex.ru>
2020-07-04 05:26:27 +0300

    add cycle gan option

--- cyclegan/test_image.py
+++ cyclegan/test_image.py
@@ -4,7 +4,7 @@ from cyclegan.cyclegan_pytorch.models import Generator
 
 model = Generator().to('cuda')
 
-model.load_state_dict(torch.load("cyclegan/weights/cezanne2photo/netG_A2B.pth"))
+model.load_state_dict(torch.load("cyclegan/weights/cezanne2photo/netG_B2A.pth"))
 
 model.eval()
 
