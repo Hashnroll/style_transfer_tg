2742b43635377fb01dbbaa9421a3d068d4f27f07
Ilya Shchuka <ilya.shchuka@yandex.ru>
2020-07-04 05:35:57 +0300

    remove vangogh gan option

--- cyclegan/test_image.py
+++ cyclegan/test_image.py
@@ -4,7 +4,7 @@ from cyclegan.cyclegan_pytorch.models import Generator
 
 model = Generator().to('cuda')
 
-model.load_state_dict(torch.load("cyclegan/weights/vangogh.pth"))
+model.load_state_dict(torch.load("cyclegan/weights/cezanne.pth"))
 
 model.eval()
 
